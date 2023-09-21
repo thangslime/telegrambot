60d84f89236d441f78ee0cf314ff66cc887e502a
Nguyen Anh Thang <thangkfc@gmail.com>
2023-09-21 10:46:17 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const routes = require("./router");
+const routes = require("../src/router");
 const app = express();
 
 // * Cors
