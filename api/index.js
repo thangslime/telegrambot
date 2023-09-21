882df93b82ab3df2488dda0cf2cb724ffbbd9816
Nguyen Anh Thang <thangkfc@gmail.com>
2023-09-21 11:01:32 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,20 +1,17 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const app = express();
 
-// * Cors
-app.use(cors());
-
-// * Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 // * Api routes
-app.get('/api', (req, res) => {
+app.get('/test', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.end(`Hello! Go to item: 1`);
 });
+
+app.post('/webhook', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(res.body);
+})
 
 module.exports = app
