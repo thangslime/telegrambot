00cf0f67a039f271fa4359c00136a8dd40caca76
Nguyen Anh Thang <thangkfc@gmail.com>
2023-09-21 10:52:46 +0700

    update

--- api/index.js
+++ api/index.js
@@ -13,5 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // * Api routes
 app.use("/api", routes);
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
 module.exports = app
