e516762655c1371b17016c4466abe1eb17cee566
Nguyen Anh Thang <thangkfc@gmail.com>
2023-09-21 13:43:38 +0700

    update

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ app.get('/', (req, res) => {
 app.post('/webhook', async (req, res) => {
     const body = req.body.message;
     console.log(body);
-    if (body.new_chat_member) {
-      const newMember = body.new_chat_member;
-      const user_name = `${newMember.first_name ? newMember.first_name : ''} ${newMember.last_name ? newMember.last_name : ''}`.trim()
-      const data = {
-        chat_id: `@${body.chat.username}`,
-        text: `Hi <i><b>${user_name}</b></i>,\nChào mừng bạn đến với <strong>${body.chat.title}</strong>.\nChúc bạn may mắn.`,
-        parse_mode: "HTML"
-      };
+    // if (body.new_chat_member) {
+    //   const newMember = body.new_chat_member;
+    //   const user_name = `${newMember.first_name ? newMember.first_name : ''} ${newMember.last_name ? newMember.last_name : ''}`.trim()
+    //   const data = {
+    //     chat_id: `@${body.chat.username}`,
+    //     text: `Hi <i><b>${user_name}</b></i>,\nChào mừng bạn đến với <strong>${body.chat.title}</strong>.\nChúc bạn may mắn.`,
+    //     parse_mode: "HTML"
+    //   };
 
-      await axios.post(
-        `https://api.telegram.org/bot${process.env.BOT_APIKEY || '6547009775:AAHu9R8o27SiqKBfe4qY9RPWH5CdtzB2kl8'}/sendMessage`,
-        data
-      );
-    } else {
-        const data = {
-            chat_id: `@${body.chat.username}`,
-            text: `Dìa dia 🤘🤘🤘!!!`,
-            parse_mode: "HTML"
-          };
+    //   await axios.post(
+    //     `https://api.telegram.org/bot${process.env.BOT_APIKEY || '6547009775:AAHu9R8o27SiqKBfe4qY9RPWH5CdtzB2kl8'}/sendMessage`,
+    //     data
+    //   );
+    // } else {
+    //     const data = {
+    //         chat_id: `@${body.chat.username}`,
+    //         text: `Dìa dia 🤘🤘🤘!!!`,
+    //         parse_mode: "HTML"
+    //       };
     
-          await axios.post(
-            `https://api.telegram.org/bot${process.env.BOT_APIKEY || '6547009775:AAHu9R8o27SiqKBfe4qY9RPWH5CdtzB2kl8'}/sendMessage`,
-            data
-          );
-    }
+    //       await axios.post(
+    //         `https://api.telegram.org/bot${process.env.BOT_APIKEY || '6547009775:AAHu9R8o27SiqKBfe4qY9RPWH5CdtzB2kl8'}/sendMessage`,
+    //         data
+    //       );
+    // }
   res.status(200).json({success: true, dataBody: req.body})
 })
 
