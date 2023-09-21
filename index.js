5618029a6b157410d92715e4916161c4313e33e8
Nguyen Anh Thang <thangkfc@gmail.com>
2023-09-21 11:29:09 +0700

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.post('/webhook', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(req.body)
+  res.status(200).json({success: true})
 })
 
 // Export the Express API
