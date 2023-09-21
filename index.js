2ff641e7815559d4d710daeaa4354aaaed0dc003
Nguyen Anh Thang <thangkfc@gmail.com>
2023-09-21 14:28:11 +0700

    update

--- index.js
+++ index.js
@@ -1,51 +1,66 @@
 // index.js
-const express = require('express')
+const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
-const app = express()
-const PORT = 8443
+const app = express();
+const PORT = 8443;
 
 // * Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
-app.post('/webhook', async (req, res) => {
-    const body = req.body.message;
-    console.log(body);
-    if (body.new_chat_member) {
-      const newMember = body.new_chat_member;
-      const user_name = `${newMember.first_name ? newMember.first_name : ''} ${newMember.last_name ? newMember.last_name : ''}`.trim()
-      const data = {
-        chat_id: `@${body.chat.username}`,
-        text: `Hi <i><b>${user_name}</b></i>,\nChào mừng bạn đến với <strong>${body.chat.title}</strong>.\nChúc bạn may mắn.`,
-        parse_mode: "HTML"
-      };
-
-      await axios.post(
-        `https://api.telegram.org/bot${process.env.BOT_APIKEY || '6468513372:AAFVyJWK7R0lQ5CkYGPf0-t_hAR_qgjOF1o'}/sendMessage`,
-        data
-      );
-    } else {
-        const data = {
+app.post("/webhook", async (req, res) => {
+    try {
+        const body = req.body.message;
+        console.log(body);
+        if (body.new_chat_member) {
+            const newMember = body.new_chat_member;
+            const user_name = `${newMember.first_name ? newMember.first_name : ""} ${
+            newMember.last_name ? newMember.last_name : ""
+            }`.trim();
+            const data = {
             chat_id: `@${body.chat.username}`,
-            text: `Dìa dia 🤘🤘🤘!!!`,
-            parse_mode: "HTML"
-          };
-    
-          await axios.post(
-            `https://api.telegram.org/bot${process.env.BOT_APIKEY || '6468513372:AAFVyJWK7R0lQ5CkYGPf0-t_hAR_qgjOF1o'}/sendMessage`,
+            text: `Hi <i><b>${user_name}</b></i>,\nChào mừng bạn đến với <strong>${body.chat.title}</strong>.\nChúc bạn may mắn.`,
+            parse_mode: "HTML",
+            };
+
+            await axios.post(
+            `https://api.telegram.org/bot${
+                process.env.BOT_APIKEY ||
+                "6468513372:AAFVyJWK7R0lQ5CkYGPf0-t_hAR_qgjOF1o"
+            }/sendMessage`,
             data
-          );
+            );
+        } else {
+            if (body.chat.type != 'private') {
+                const data = {
+                    chat_id: `@${body.chat.username}`,
+                    text: `Dìa dia 🤘🤘🤘!!!`,
+                    parse_mode: "HTML",
+                    };
+        
+                    await axios.post(
+                    `https://api.telegram.org/bot${
+                        process.env.BOT_APIKEY ||
+                        "6468513372:AAFVyJWK7R0lQ5CkYGPf0-t_hAR_qgjOF1o"
+                    }/sendMessage`,
+                    data
+                    );
+            }
+        }
+        res.status(200).json({ success: true, dataBody: req.body });
+    } catch (error) {
+        console.log(error);
+
     }
-  res.status(200).json({success: true, dataBody: req.body})
-})
+});
 
 app.listen(PORT, () => {
-    console.log(`API listening on PORT ${PORT} `)
-  })
+  console.log(`API listening on PORT ${PORT} `);
+});
 // Export the Express API
-module.exports = app
+module.exports = app;
