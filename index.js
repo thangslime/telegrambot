2bd15b3312f720c20fcb87760f6f9bfd326186d0
Nguyen Anh Thang <thangkfc@gmail.com>
2023-09-21 11:33:52 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // index.js
 const express = require('express')
 const axios = require("axios");
-
+const bodyParser = require("body-parser");
 const app = express()
 const PORT = 8443
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+// * Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
@@ -15,8 +15,11 @@ app.get('/', (req, res) => {
 
 app.post('/webhook', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.status(200).json({success: true})
+  res.status(200).json({success: true, dataBody: req.body})
 })
 
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
+  })
 // Export the Express API
 module.exports = app
