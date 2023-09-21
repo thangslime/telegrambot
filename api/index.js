c5fe7e78c63d859a6b696170c3f3a09d5ae5f99c
Nguyen Anh Thang <thangkfc@gmail.com>
2023-09-21 11:09:23 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,16 +1,32 @@
-const express = require("express");
-const app = express();
+import { NowRequest, NowResponse } from "@vercel/node";
+import axios from "axios";
 
-// * Api routes
-app.get('/test', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: 1`);
-});
+export default async (req, res) => {
+  try {
+    const body = req.body.message;
+    console.log(body);
+    // const teleToken = req.query.token;
+    // if (body.new_chat_member) {
+    //   const newMember = body.new_chat_member;
+    //   const data = {
+    //     chat_id: `@${body.chat.username}`,
+    //     text: `Hi <i><b>${parseUsername(newMember.first_name, newMember.last_name)}</b></i>,\nChào mừng bạn đến với <strong>${body.chat.title}</strong>.\nChúc bạn chửa hoang thành công.`,
+    //     parse_mode: "HTML"
+    //   };
 
-app.post('/webhook', (req, res) => {
-    res.status = 200;
-    res.json(req.body);
-})
+    //   await axios.post(
+    //     `https://api.telegram.org/bot${teleToken}/sendMessage`,
+    //     data
+    //   );
+    // }
 
-module.exports = app
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: false });
+  }
+};
+
+function parseUsername(first_name, last_name) {
+  return `${first_name ? first_name : ''} ${last_name ? last_name : ''}`.trim();
+}
