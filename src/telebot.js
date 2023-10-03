4e9f5767847d0288c237f54b013f4fd473be38b6
Nguyen Anh Thang <thangkfc@gmail.com>
2023-10-03 15:57:39 +0700

    update

--- src/telebot.js
+++ src/telebot.js
@@ -47,7 +47,7 @@ const importWallet = async (bot, chat_id, data) => {
         return true
     } catch (error) {
         console.log(error);
-        bot.sendMessage(chat_id, JSON.stringify(error));
+        bot.sendMessage(chat_id, error.message);
     }
 }
 
