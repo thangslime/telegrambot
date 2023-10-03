673756824e729e651275a58e0ac1f141f373d9e6
Nguyen Anh Thang <thangkfc@gmail.com>
2023-10-03 15:01:23 +0700

    update

--- index.js
+++ index.js
@@ -112,9 +112,9 @@ app.get("/", (req, res) => {
   </html>`);
 });
 
-app.post("/webhook", async (req, res) => {
+app.post("/webhook", (req, res) => {
   try {
-    await bot.processUpdate(req.body);
+    bot.processUpdate(req.body);
     res.status(200).json({ success: true, dataBody: req.body });
   } catch (error) {
     console.log(error);
