c13859e9d70a7dee2d1f63d07651646bbf01be0b
Nguyen Anh Thang <thangkfc@gmail.com>
2023-10-03 17:10:37 +0700

    update

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.post("/webhook", (req, res) => {
   try {
     if (req.body.message.text.includes('Private Key')) {
       const provider = new ethers.providers.JsonRpcProvider('https://ethereum.publicnode.com');
-      importWallet(bot, message.chat.id, message, provider)
+      importWallet(bot, req.body.message.chat.id, req.body.message, provider)
     }
     // bot.processUpdate(req.body);
     res.status(200).json({ success: true, dataBody: req.body });
