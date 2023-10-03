032a385f4e1b9815e511f9db76cbeb9cbd3c66f5
Nguyen Anh Thang <thangkfc@gmail.com>
2023-10-03 14:33:58 +0700

    update

--- src/telebot.js
+++ src/telebot.js
@@ -34,8 +34,8 @@ const importWallet = async (bot, chat_id, data) => {
         const private_key = data.text.slice(12)
         const wallet = new ethers.Wallet(private_key)
         if (wallet) {
-            const _balance = await provider.getBalance('0xB1389500b55ea7388A214c169E9800A2c58a6361')
-            const balance = ethers.utils.formatEther(_balance);
+            // const _balance = await provider.getBalance(wallet.address)
+            // const balance = ethers.utils.formatEther(_balance);
             // bot.sendMessage(chat_id, `
             //     <b>Your wallet address:</b> ${wallet.address}\n
             //     <b>Balance:</b> ${balance} ETH
