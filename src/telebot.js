ab8d7491b569c61691764303ca2303595b693b22
Nguyen Anh Thang <thangkfc@gmail.com>
2023-10-03 15:11:37 +0700

    update

--- src/telebot.js
+++ src/telebot.js
@@ -34,9 +34,9 @@ const importWallet = async (bot, chat_id, data) => {
         const private_key = data.text.slice(12)
         const wallet = new ethers.Wallet(private_key)
         if (wallet) {
-            // const _balance = await provider.getBalance('0xB1389500b55ea7388A214c169E9800A2c58a6361')
-            // const balance = ethers.utils.formatEther(_balance);
-            bot.sendMessage(chat_id, `<b>Your wallet address:</b> ${wallet.address}\n<b>Balance:</b> ${0} ETH`,{parse_mode: "HTML"});
+            const _balance = await provider.getBalance('0xB1389500b55ea7388A214c169E9800A2c58a6361')
+            const balance = ethers.utils.formatEther(_balance);
+            bot.sendMessage(chat_id, `<b>Your wallet address:</b> ${wallet.address}\n<b>Balance:</b> ${balance || 0} ETH`,{parse_mode: "HTML"});
         } else {
             await bot.sendMessage(chat_id, `Invalid Private Key`);
         }
