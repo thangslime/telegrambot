b963fdd3e9300717fcd8b3c10e4b41e3816cfcaf
Nguyen Anh Thang <thangkfc@gmail.com>
2023-10-03 15:40:17 +0700

    upadte

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ app.get("/", (req, res) => {
 app.post("/webhook", (req, res) => {
   try {
     bot.processUpdate(req.body);
+    res.status(200).json({ success: true, dataBody: req.body });
   } catch (error) {
     console.log(error);
   }
